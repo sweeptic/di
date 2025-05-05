8d55da9db7b21277a9087eabd4137ea2e0871861
Attila Horvath <sweeptic@gmail.com>
2025-05-05 15:28:00 +0200

    04. More on DI Between Modules

--- cpu/cpu.service.ts
+++ cpu/cpu.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'power/power.service';
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+  constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('Drawing 10watts of power from Power Service');
+    this.powerService.supplyPower(10);
+
+    return a + b;
+  }
+}
--- disk/disk.module.ts
+++ disk/disk.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DiskService } from './disk.service';
+import { PowerModule } from 'power/power.module';
 
 @Module({
-  providers: [DiskService]
+  providers: [DiskService],
+  imports: [PowerModule],
 })
 export class DiskModule {}
